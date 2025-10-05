4da7209173dee1609e3a4dae3353407ef0880898
SaltineEnthusiast <svargasschool@gmail.com>
2025-10-04 23:05:04 -0700

    a

--- CorpusCallosumPlugin.cs
+++ CorpusCallosumPlugin.cs
@@ -1,7 +1,10 @@
 using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
+using UnityEngine;
 
 namespace Corpus_Callosum;
+// Open Assembly-CSharp in Object Browser
 
 // TODO - adjust the plugin guid as needed
 [BepInAutoPlugin(id: "io.github.corpus_callosum")]
@@ -9,9 +12,26 @@ public partial class CorpusCallosumPlugin : BaseUnityPlugin
 {
     private void Awake()
     {
-        // Put your initialization logic here
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
         
         Harmony.CreateAndPatchAll(typeof(CorpusCallosumPlugin));
     }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(HeroController), "Dash")]
+    private static void DashPostfix(HeroController __instance) {
+        if (__instance.warriorState.IsInRageMode)
+        {
+            __instance.dash_timer = 0f;
+        }
+    }
+    
+    /*
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ClassWithFunctionYouWant), "name of the function you want)]
+    private static void PreferablyWhateverTheFunctionIsCalledAppendedByPostfix(ClassWithFunctionYouWant __instance, any other variables)
+    {
+        //__instance is how you refer to things within ClassWithFunctionYouWant
+    }
+    */
 }
