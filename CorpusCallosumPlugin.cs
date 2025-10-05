9d2ab623447e16415e5eeb566e6b55f6be337a6b
SaltineEnthusiast <svargasschool@gmail.com>
2025-10-05 14:55:46 -0700

    10/5/25 1

--- CorpusCallosumPlugin.cs
+++ CorpusCallosumPlugin.cs
@@ -1,6 +1,8 @@
 using BepInEx;
 using BepInEx.Logging;
+using GlobalEnums;
 using HarmonyLib;
+using System;
 using UnityEngine;
 
 namespace Corpus_Callosum;
@@ -15,17 +17,30 @@ public partial class CorpusCallosumPlugin : BaseUnityPlugin
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
         
         Harmony.CreateAndPatchAll(typeof(CorpusCallosumPlugin));
+        if (!Enum.IsDefined(typeof(CrestType), "Weaver"))
+        {
+            int NewValue = Enum.GetValues(typeof(CrestType)).Length;
+            CrestType Weaver = (CrestType)NewValue;
+            Logger.LogInfo($"{Name}({Id}) Weaver crest has loaded");
+        }
+        else
+        {
+            Logger.LogInfo($"{Name}({Id}) Weaver crest couldn't load");
+        }
+        Logger.LogInfo($"Crest list: {Enum.GetValues(typeof(CrestType))}");
     }
 
+    /*
     [HarmonyPostfix]
     [HarmonyPatch(typeof(HeroController), "Dash")]
-    private static void DashPostfix(HeroController __instance) {
+    private static void DashPostfix(HeroController __instance)
+    {
         if (__instance.warriorState.IsInRageMode)
         {
-            __instance.dash_timer = 0f;
+            __instance.AddCurrency(10, CurrencyType.Money, true); // Adds 70 for some reason
         }
-    }
-    
+    }*/
+
     /*
     [HarmonyPostfix]
     [HarmonyPatch(typeof(ClassWithFunctionYouWant), "name of the function you want)]
